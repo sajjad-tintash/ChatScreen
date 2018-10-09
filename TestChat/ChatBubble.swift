6e724916fd98aaf9e7de6af073a7deefc819b65a
Sajjad-tintash <sajjad@tintash.com>
2018-10-09 16:42:55 +0500

    Added project

---
+++ TestChat/ChatBubble.swift
@@ -0,0 +1,126 @@
+//
+//  ChatBubble.swift
+//  TestChat
+//
+//  Created by Sajjad  on 09/10/2018.
+//  Copyright © 2018 Sajjad . All rights reserved.
+//
+
+import UIKit
+
+let MIN_BUBBLE_HEIGHT   : CGFloat = 32
+let MAX_BUBBLE_WIDTH    : CGFloat = UIScreen.main.bounds.width * 0.6
+let SCREEN_WIDTH        : CGFloat = UIScreen.main.bounds.width
+let BUBBLE_X_PADDING    : CGFloat = 28
+let BUBBLE_Y_PADDING    : CGFloat = 8
+let BUBBLE_SENDER_SPACE : CGFloat = 40
+let FONT                : UIFont  = UIFont.systemFont(ofSize: 18)
+
+
+
+class ChatBubble: UITableViewCell {
+
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var senderImage: UIImageView!
+    
+    var outgoingMessageLayer: CAShapeLayer? = nil
+    var dataText: String? = nil
+    var label: UILabel? = nil
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        backgroundColor = .clear
+        backgroundView = nil
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        if outgoingMessageLayer != nil {
+            outgoingMessageLayer?.removeFromSuperlayer()
+            outgoingMessageLayer = nil
+        }
+        if dataText != nil {
+            dataText = nil
+        }
+        if label != nil {
+            label?.removeFromSuperview()
+            label = nil
+        }
+    }
+    
+    func setup(_ text: String) {
+        dataText = text
+        senderImage.layer.cornerRadius = senderImage.frame.width/2
+        senderImage.clipsToBounds = true
+        showBubble()
+    }
+
+    
+    
+    func showBubble() {
+        guard let text = dataText else { return }
+        
+        
+        var height = text.height(withConstrainedWidth: MAX_BUBBLE_WIDTH, font: FONT)
+        var width = MAX_BUBBLE_WIDTH
+        
+        if height < MIN_BUBBLE_HEIGHT {
+            height = MIN_BUBBLE_HEIGHT
+            width = text.width(withConstrainedHeight: height, font: FONT)
+        }
+        
+        label =  UILabel()
+        label?.numberOfLines = 0
+        label?.font = FONT
+        label?.textColor = .white
+        label?.textAlignment = .right
+        label?.text = text
+        
+        label?.frame = CGRect(x: SCREEN_WIDTH - (width + BUBBLE_X_PADDING/2) - BUBBLE_SENDER_SPACE,y:2,
+                             width: width,
+                             height: height)
+        
+        height += BUBBLE_Y_PADDING
+        width  += BUBBLE_X_PADDING
+        
+        let bezierPath = UIBezierPath()
+        bezierPath.move(to: CGPoint(x: width - 22, y: height))
+        bezierPath.addLine(to: CGPoint(x: 17, y: height))
+        bezierPath.addCurve(to: CGPoint(x: 0, y: height - 17), controlPoint1: CGPoint(x: 7.61, y: height), controlPoint2: CGPoint(x: 0, y: height - 7.61))
+        bezierPath.addLine(to: CGPoint(x: 0, y: 17))
+        bezierPath.addCurve(to: CGPoint(x: 17, y: 0), controlPoint1: CGPoint(x: 0, y: 7.61), controlPoint2: CGPoint(x: 7.61, y: 0))
+        bezierPath.addLine(to: CGPoint(x: width - 21, y: 0))
+        bezierPath.addCurve(to: CGPoint(x: width - 4, y: 17), controlPoint1: CGPoint(x: width - 11.61, y: 0), controlPoint2: CGPoint(x: width - 4, y: 7.61))
+        bezierPath.addLine(to: CGPoint(x: width - 4, y: height - 11))
+        bezierPath.addCurve(to: CGPoint(x: width, y: height), controlPoint1: CGPoint(x: width - 4, y: height - 1), controlPoint2: CGPoint(x: width, y: height))
+        bezierPath.addLine(to: CGPoint(x: width + 0.05, y: height - 0.01))
+        bezierPath.addCurve(to: CGPoint(x: width - 11.04, y: height - 4.04), controlPoint1: CGPoint(x: width - 4.07, y: height + 0.43), controlPoint2: CGPoint(x: width - 8.16, y: height - 1.06))
+        bezierPath.addCurve(to: CGPoint(x: width - 22, y: height), controlPoint1: CGPoint(x: width - 16, y: height), controlPoint2: CGPoint(x: width - 19, y: height))
+        bezierPath.close()
+        
+        outgoingMessageLayer = CAShapeLayer()
+        outgoingMessageLayer?.path = bezierPath.cgPath
+        outgoingMessageLayer?.frame = CGRect(x: SCREEN_WIDTH - width - BUBBLE_SENDER_SPACE,y:0,
+                                                         width: width,
+                                                         height: height)
+        outgoingMessageLayer?.fillColor = UIColor.random().cgColor
+        
+        if let bubbleLayer = outgoingMessageLayer {
+            mainView.layer.addSublayer(bubbleLayer)
+        }
+        if let textlabel = label {
+            mainView.addSubview(textlabel)
+            mainView.addConstraint(NSLayoutConstraint(item: textlabel, attribute: .top, relatedBy: .equal, toItem: mainView, attribute: .top, multiplier: 1, constant: BUBBLE_Y_PADDING/2))
+            mainView.addConstraint(NSLayoutConstraint(item: textlabel, attribute: .bottom, relatedBy: .equal, toItem: mainView, attribute: .bottom, multiplier: 1, constant: -BUBBLE_Y_PADDING/2))
+        }
+
+    }
+}
---
+++ TestChat/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  TestChat
+//
+//  Created by Sajjad  on 09/10/2018.
+//  Copyright © 2018 Sajjad . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let dataSource : [String] = ["App Design","App layout - Backend - Responsive design","The purpose of this product specification is to product an app that host text style chat stories similar to Hooked by Telepathic, Yarn by Science Mobile & Tap by Wattpad.","The ranking of series depends on which section we are looking at. For example, the carousel section will mostly likely be a curated list and the order of series is the same for all users. We will change the content and the order of the list manually from time to time. Later we will develop logic on how the series should be ranked, could be a combination of user data, popularity, publishing date and manual curation","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."]
+    
+    
+    func setup(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = tableView.frame.height
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.register(UINib(nibName: "ChatBubble", bundle: nil), forCellReuseIdentifier: "ChatBubble")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        setup()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+extension ViewController: UITableViewDelegate {
+}
+
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ChatBubble") as? ChatBubble else {
+            let cell = UITableViewCell()
+            cell.backgroundColor = .black
+            return cell
+        }
+        cell.setup(dataSource[indexPath.row])
+        return cell
+    }
+}
+
+
+
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
+        
+        return ceil(boundingBox.height)
+    }
+    
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
+        
+        return ceil(boundingBox.width)
+    }
+}
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(red:   .random(),
+                       green: .random(),
+                       blue:  .random(),
+                       alpha: 1.0)
+    }
+}
