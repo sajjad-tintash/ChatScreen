1bf3c42590a23b11ea2400bf70e704bf83bacf98
Sajjad-tintash <sajjad@tintash.com>
2018-10-10 15:26:42 +0500

    Code Refactor and Clean Up

--- TestChat/ChatBubble.swift
+++ TestChat/ChatBubble.swift
@@ -62,7 +62,7 @@ class ChatBubble: UITableViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        dataText = nil
+        dataText     = nil
         imageContent = nil
         if messageLayer != nil {
             messageLayer?.removeFromSuperlayer()
@@ -94,12 +94,62 @@ class ChatBubble: UITableViewCell {
     
     
     func showBubble() {
-        var height: CGFloat
-        var width: CGFloat
-        var bezierPath: UIBezierPath
+        var height      : CGFloat
+        var width       : CGFloat
+        var bezierPath  : UIBezierPath
         switch mediaType {
         case .image:
             guard let image = imageContent else { return }
+            var messageLayer : CAShapeLayer
+            (height,width)  = getHeightAndWidth(image)
+            bezierPath      = getBezierPath(width: width, height: height)
+            messageLayer    = getShapeLayer(bezierPath, width: width, height: height)
+            chatImageView   = getBubbleImageView(image, width: width, height: height)
+            chatImageView?.layer.mask = messageLayer
+            if let chatImageView = chatImageView {
+                mainView.addSubview(chatImageView)
+                mainView.addConstraint(getConstraint(item: chatImageView, attribute: .top, toItem: mainView))
+                mainView.addConstraint(getConstraint(item: chatImageView, attribute: .bottom, toItem: mainView))
+            }
+        case .text:
+            guard let text  = dataText else { return }
+            (height,width)  = getHeightAndWidth(text)
+            label           = getTextLabel(text, width: width, height: height)
+            height         += BUBBLE_Y_PADDING
+            width          += BUBBLE_X_PADDING
+            bezierPath      = getBezierPath(width: width, height: height)
+            messageLayer    = getShapeLayer(bezierPath, width: width, height: height)
+            if let messageLayer = messageLayer {
+                mainView.layer.addSublayer(messageLayer)
+            }
+            if let textlabel = label {
+                mainView.addSubview(textlabel)
+                mainView.addConstraint(getConstraint(item: textlabel, attribute: .top, toItem: mainView))
+                mainView.addConstraint(getConstraint(item: textlabel, attribute: .bottom, toItem: mainView))
+            }
+        }
+    }
+}
+
+
+//MARK:- Top Level Getters
+extension ChatBubble {
+    
+    func getHeightAndWidth<T>(_ data: T) -> (CGFloat,CGFloat) {
+        var height  : CGFloat
+        var width   : CGFloat
+        switch mediaType {
+        case .text:
+            guard let text = data as? String else { return (0,0) }
+            height = text.height(withConstrainedWidth: MAX_BUBBLE_WIDTH, font: FONT)
+            width = MAX_BUBBLE_WIDTH
+            
+            if height < MIN_BUBBLE_HEIGHT {
+                height = MIN_BUBBLE_HEIGHT
+                width = text.width(withConstrainedHeight: height, font: FONT)
+            }
+        case .image:
+            guard let image = data as? UIImage else { return (0,0) }
             let aspectRatio = image.size.width / image.size.height
             if image.size.width < MAX_BUBBLE_WIDTH {
                 width = image.size.width
@@ -112,127 +162,65 @@ class ChatBubble: UITableViewCell {
                     width = height * aspectRatio
                 }
             }
-            switch chatType {
-            case .sender:
-                bezierPath = senderBazierPath(width: width, height: height)
-                let messageLayer = CAShapeLayer()
-                messageLayer.path = bezierPath.cgPath
-                messageLayer.frame = CGRect(x: 0,y:0,
-                                            width: width,
-                                            height: height)
-                chatImageView = UIImageView(image: image)
-                chatImageView?.frame = CGRect(x: SCREEN_WIDTH - width - BUBBLE_SENDER_SPACE, y: 0, width: width, height: height)
-                chatImageView?.contentMode = .scaleAspectFill
-                chatImageView?.clipsToBounds = true
-                chatImageView?.layer.mask = messageLayer
-                if let chatImageView = chatImageView {
-                    mainView.addSubview(chatImageView)
-                    let constraintTop = NSLayoutConstraint(item: chatImageView, attribute: .top, relatedBy: .equal, toItem: mainView, attribute: .top, multiplier: 1, constant: BUBBLE_Y_PADDING/2)
-                    let constraintBottom = NSLayoutConstraint(item: chatImageView, attribute: .bottom, relatedBy: .equal, toItem: mainView, attribute: .bottom, multiplier: 1, constant: -BUBBLE_Y_PADDING/2)
-                    constraintTop.priority = .defaultHigh
-                    constraintBottom.priority = .defaultHigh
-                    mainView.addConstraint(constraintTop)
-                    mainView.addConstraint(constraintBottom)
-                }
-             
-
-            case .recepient:
-                bezierPath = recieverBazierPath(width: width, height: height)
-                let messageLayer = CAShapeLayer()
-                 messageLayer.path = bezierPath.cgPath
-                messageLayer.frame = CGRect(x: 0,y:0,
-                                            width: width,
-                                            height: height)
-                chatImageView = UIImageView(image: image)
-                chatImageView?.frame = CGRect(x: BUBBLE_SENDER_SPACE, y: 0, width: width, height: height)
-                chatImageView?.contentMode = .scaleAspectFill
-                chatImageView?.clipsToBounds = true
-                chatImageView?.layer.mask = messageLayer
-                
-                if let chatImageView = chatImageView {
-                    mainView.addSubview(chatImageView)
-                    let constraintTop = NSLayoutConstraint(item: chatImageView, attribute: .top, relatedBy: .equal, toItem: mainView, attribute: .top, multiplier: 1, constant: BUBBLE_Y_PADDING/2)
-                    let constraintBottom = NSLayoutConstraint(item: chatImageView, attribute: .bottom, relatedBy: .equal, toItem: mainView, attribute: .bottom, multiplier: 1, constant: -BUBBLE_Y_PADDING/2)
-                    constraintTop.priority = .defaultHigh
-                    constraintBottom.priority = .defaultHigh
-                    mainView.addConstraint(constraintTop)
-                    mainView.addConstraint(constraintBottom)
-                }
-            }
-            
-        case .text:
-            guard let text = dataText else { return }
-            height = text.height(withConstrainedWidth: MAX_BUBBLE_WIDTH, font: FONT)
-            width = MAX_BUBBLE_WIDTH
-            
-            if height < MIN_BUBBLE_HEIGHT {
-                height = MIN_BUBBLE_HEIGHT
-                width = text.width(withConstrainedHeight: height, font: FONT)
-            }
-            label =  UILabel()
-            label?.numberOfLines = 0
-            label?.font = FONT
-            switch chatType {
-            case .sender:
-                label?.textColor = .white
-                label?.textAlignment = .right
-                label?.text = text
-                label?.frame = CGRect(x: SCREEN_WIDTH - (width + BUBBLE_X_PADDING/2) - BUBBLE_SENDER_SPACE,y:2,
-                                      width: width,
-                                      height: height)
-                height += BUBBLE_Y_PADDING
-                width  += BUBBLE_X_PADDING
-                
-                bezierPath = senderBazierPath(width: width, height: height)
-                messageLayer = CAShapeLayer()
-                messageLayer?.path = bezierPath.cgPath
-                messageLayer?.frame = CGRect(x: SCREEN_WIDTH - width - BUBBLE_SENDER_SPACE,y:0,
-                                                     width: width,
-                                                     height: height)
-                messageLayer?.fillColor = UIColor.green.cgColor
-                if let messageLayer = messageLayer {
-                    mainView.layer.addSublayer(messageLayer)
-                }
-                if let textlabel = label {
-                    mainView.addSubview(textlabel)
-                    let constraintTop = NSLayoutConstraint(item: textlabel, attribute: .top, relatedBy: .equal, toItem: mainView, attribute: .top, multiplier: 1, constant: BUBBLE_Y_PADDING/2)
-                    constraintTop.priority = .defaultHigh
-                    let constraintBottom = NSLayoutConstraint(item: textlabel, attribute: .bottom, relatedBy: .equal, toItem: mainView, attribute: .bottom, multiplier: 1, constant: -BUBBLE_Y_PADDING/2)
-                    constraintBottom.priority = .defaultHigh
-                    mainView.addConstraint(constraintTop)
-                    mainView.addConstraint(constraintBottom)
-                }
-            case .recepient:
-                label?.textColor = .black
-                label?.textAlignment = .left
-                label?.text = text
-                label?.frame = CGRect(x: BUBBLE_SENDER_SPACE + BUBBLE_X_PADDING / 2,y:2,
-                                      width: width,
-                                      height: height)
-                height += BUBBLE_Y_PADDING
-                width  += BUBBLE_X_PADDING
-                bezierPath = recieverBazierPath(width: width, height: height)
-                messageLayer = CAShapeLayer()
-                messageLayer?.path = bezierPath.cgPath
-                messageLayer?.frame = CGRect(x: BUBBLE_SENDER_SPACE, y:0,
-                                                    width: width,
-                                                    height: height)
-                messageLayer?.fillColor = UIColor.lightGray.cgColor
-                if let messageLayer = messageLayer {
-                    mainView.layer.addSublayer(messageLayer)
-                }
-                if let textlabel = label {
-                    mainView.addSubview(textlabel)
-                    let constraintTop = NSLayoutConstraint(item: textlabel, attribute: .top, relatedBy: .equal, toItem: mainView, attribute: .top, multiplier: 1, constant: BUBBLE_Y_PADDING/2)
-                    constraintTop.priority = .defaultHigh
-                    let constraintBottom = NSLayoutConstraint(item: textlabel, attribute: .bottom, relatedBy: .equal, toItem: mainView, attribute: .bottom, multiplier: 1, constant: -BUBBLE_Y_PADDING/2)
-                    constraintBottom.priority = .defaultHigh
-                    mainView.addConstraint(constraintTop)
-                    mainView.addConstraint(constraintBottom)
-                }
-            }
         }
+        return (height,width)
     }
+    
+    func getConstraint(item: Any, attribute: NSLayoutAttribute, toItem: Any?) -> NSLayoutConstraint {
+        let constraint = NSLayoutConstraint(item: item, attribute: attribute, relatedBy: .equal, toItem: toItem, attribute: attribute, multiplier: 1, constant: (attribute == .top ? BUBBLE_Y_PADDING/2 : -BUBBLE_Y_PADDING/2))
+        constraint.priority = .defaultHigh
+        return constraint
+    }
+    
+    func getBezierPath(width: CGFloat, height: CGFloat) -> UIBezierPath {
+        return (chatType == .sender) ? senderBazierPath(width: width, height: height) : recieverBazierPath(width: width, height: height)
+    }
+}
+
+
+//MARK:- Helper Methods
+extension ChatBubble {
+    func getShapeLayer(_ path: UIBezierPath, width: CGFloat, height: CGFloat) -> CAShapeLayer {
+        let shapeLayer  = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        switch mediaType {
+        case .text:
+            shapeLayer.frame = chatType == .sender ? CGRect(x: SCREEN_WIDTH - width - BUBBLE_SENDER_SPACE, y:0,
+                                                            width: width,
+                                                            height: height) : CGRect(x: BUBBLE_SENDER_SPACE, y:2,
+                                                                                     width: width,
+                                                                                     height: height)
+            shapeLayer.fillColor = chatType == .sender ? UIColor.green.cgColor : UIColor.lightGray.cgColor
+        case .image:
+            shapeLayer.frame = CGRect(x: 0,y:0, width: width, height: height)
+        }
+        return shapeLayer
+    }
+    
+    func getBubbleImageView(_ image: UIImage, width: CGFloat, height: CGFloat) -> UIImageView {
+        let bubbleImageView             = UIImageView(image: image)
+        bubbleImageView.frame           = CGRect(x: (chatType == .sender ? SCREEN_WIDTH - width - BUBBLE_SENDER_SPACE : BUBBLE_SENDER_SPACE), y:                0, width: width, height: height)
+        bubbleImageView.contentMode     = .scaleAspectFill
+        bubbleImageView.clipsToBounds   = true
+        return bubbleImageView
+    }
+    
+    func getTextLabel(_ text: String, width: CGFloat, height: CGFloat) -> UILabel {
+        let label           =  UILabel()
+        label.numberOfLines = 0
+        label.font          = FONT
+        label.textColor     = .white
+        label.textAlignment = chatType == .sender ? .right : .left
+        label.text          = text
+        label.frame = CGRect(x: chatType == .sender ? (SCREEN_WIDTH - (width + BUBBLE_X_PADDING/2) - BUBBLE_SENDER_SPACE) : (BUBBLE_SENDER_SPACE +    BUBBLE_X_PADDING / 2),y:2,
+                             width: width,
+                             height: height)
+        return label
+    }
+}
+
+//MARK:- Bezier Getters
+extension ChatBubble {
     
     func senderBazierPath(width: CGFloat, height: CGFloat) -> UIBezierPath {
         let bezierPath = UIBezierPath()
