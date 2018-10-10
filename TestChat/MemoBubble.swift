c3e50593632918f9f4c776f5ed896c2b9af801a8
Salman Akram <salman.akram@tintash.com>
2018-10-10 16:01:48 +0500

    Added UI for MemoBubble

---
+++ TestChat/MemoBubble.swift
@@ -0,0 +1,24 @@
+//
+//  MemoBubble.swift
+//  TestChat
+//
+//  Created by Salman Akram on 10/10/2018.
+//  Copyright © 2018 Sajjad . All rights reserved.
+//
+
+import UIKit
+
+class MemoBubble: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
